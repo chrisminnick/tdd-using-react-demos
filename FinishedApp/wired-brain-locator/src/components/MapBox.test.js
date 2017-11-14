d95d9a53843eb5162da15cacfd1b966089eb0e92
chrisminnick <chris@watzthis.com>
2017-11-14 11:42:54 -0800

    made first enzyme test

--- FinishedApp/wired-brain-locator/src/components/MapBox.test.js
+++ FinishedApp/wired-brain-locator/src/components/MapBox.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import MapBox from './MapBox';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<MapBox />, div);
+    const component = shallow(<MapBox />);
+    expect(component).toExist();
 });
