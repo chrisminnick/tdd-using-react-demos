8a2107497127b1c69b871f2916516d3d0a99bba8
Pluralsight <pluralsight@wtmac.local>
2018-04-09 12:07:12 -0700

    working on adding ajax

--- FinishedApp/store-locator/src/containers/StoreLocator.js
+++ FinishedApp/store-locator/src/containers/StoreLocator.js
@@ -3,27 +3,39 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import Map from '../components/Map';
 import mapChooser from '../mapChooser';
+import axios from 'axios';
 
 class StoreLocator extends Component {
     constructor(props) {
         super(props);
+
         this.shops = [{
-                'location'  : 'Portland',
-                'address'   : '123 Portland Dr.'
-            },{
-                'location'  : 'Astoria',
-                'address'   : '123 Astoria Dr.'
-            },
+            'location'  : 'Portland',
+            'address'   : '123 Portland Dr.'
+        },{
+            'location'  : 'Astoria',
+            'address'   : '123 Astoria Dr.'
+        },
             {
                 'location'  : '',
                 'address'   : ''
             }];
+
         this.state = {
-            currentMap: 'none.png'
+            currentMap: 'none.png',
+            shops: []
         };
 
         this.chooseMap = this.chooseMap.bind(this);
 
+    }
+    componentDidMount(){
+
+        return axios.get('http://localhost:3000/data/shops.json',{adapter: require('axios/lib/adapters/http'),}).then(response => {
+            this.setState({
+                shops:response.data.shops
+            })
+        })
     }
 
     chooseMap(e){
@@ -31,17 +43,17 @@ class StoreLocator extends Component {
     }
     render() {
 
-        let storeButtons = this.shops.map((shop,id)=>{
+        let storeButtons = this.state.shops.map((shop,id)=>{
             return (<Button key={id} handleClick={this.chooseMap} location={shop.location}/>);
         });
         return (<div>
-                    <Header />
-                    <div>
-                        {storeButtons}
-                    </div>
-                    <Map imagename={this.state.currentMap} location={this.props.location}/>
+                <Header />
+                <div>
+                    {storeButtons}
                 </div>
-                );
+                <Map imagename={this.state.currentMap} location={this.props.location}/>
+            </div>
+        );
     }
 }
 
--- FinishedApp/store-locator/src/containers/__tests__/StoreLocator.test.js
+++ FinishedApp/store-locator/src/containers/__tests__/StoreLocator.test.js
@@ -2,33 +2,72 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
-describe("StoreLocator", function(){
+describe('StoreLocator', function () {
 
-let mountedStoreLocator;
-beforeEach(()=>{
-    mountedStoreLocator = shallow(<StoreLocator />);
+    let mountedStoreLocator;
+    beforeEach(() => {
+        mountedStoreLocator = shallow(<StoreLocator />);
+    });
+
+    //TODO: rewrite using this https://www.npmjs.com/package/jest-mock-axios
+
+    jest.mock('axios', ()=> {
+        const exampleShopData = [
+            {"location":"test location","address":"test address"}
+        ];
+
+        return {
+            get: jest.fn(() => Promise.resolve(exampleShopData)),
+        }
+    });
+
+
+    it('is an instance', ()=>{
+        const inst = mountedStoreLocator.instance();
+        expect(inst).toBeTruthy;
+    });
+
+    const axios = require('axios');
+
+    it('fetches articles on #componentDidMount', ()=>{
+        mountedStoreLocator.instance().componentDidMount().then(()=> {
+            //expect(axios.get).toHaveBeenCalled();
+            //expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/data/shops.json');
+            expect(mountedStoreLocator.state()).toHaveProperty('shops', [
+                {"location": "test location", "address": "test address"}
+            ]);
+            done();
+        });
+    });
+
+
+
+    it('renders without crashing', () => {
+        let mountedStoreLocator = shallow(<StoreLocator />);
+    });
+
+    it('renders a header', () => {
+        const headers = mountedStoreLocator.find('Header');
+        expect(headers.length).toBe(1);
+    });
+
+    it('renders a map', () => {
+        const maps = mountedStoreLocator.find('Map');
+        expect(maps.length).toBe(1);
+    });
+
+
+
 });
 
-it('renders without crashing', () => {
-    let mountedStoreLocator = shallow(<StoreLocator />);
-});
-
-it('renders a header', () => {
-    const headers = mountedStoreLocator.find('Header');
-    expect(headers.length).toBe(1);
-});
-
-it('renders a map', () => {
-    const maps = mountedStoreLocator.find('Map');
-    expect(maps.length).toBe(1);
-})
-});
 
 describe('chooseMap', ()=> {
-    it('updates this.state.currentMap using the location passed to it', ()=>{
-        let mountedStoreLocator = shallow(<StoreLocator />);
-        let mockEvent = {target:{value:'testland'}};
-        mountedStoreLocator.instance().chooseMap(mockEvent);
-        expect(mountedStoreLocator.instance().state.currentMap).toBe('testland.png');
-    })
-});
+        it('updates this.state.currentMap using the location passed to it', ()=>{
+            let mountedStoreLocator = shallow(<StoreLocator />);
+            let mockEvent = {target:{value:'testland'}};
+            mountedStoreLocator.instance().chooseMap(mockEvent);
+            expect(mountedStoreLocator.instance().state.currentMap).toBe('testland.png');
+        })
+    }
+);
+
